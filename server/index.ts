ad5d3bb418701eacde7a9fe6598abcb7c8d5773a
rafia9005 <rafia9005@gmail.com>
2025-02-22 12:07:44 +0700

    Init @clerk/express

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,24 @@
+import "dotenv/config";
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { requireAuth, getAuth, clerkMiddleware } from "@clerk/express";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware());
 
-app.get("/api", (req: Request, res: Response) => {
-  res.json({ message: "Hello from Express with TypeScript!" });
+app.get("/api", async (req: Request, res: Response) => {
+  const { userId } = getAuth(req);
+
+  if (!userId) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  return res.json({ message: "Success", userId });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
